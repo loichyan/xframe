820c9a08f12ea360565a295ffd06835bd96b71e1
loichyan <loichyan@foxmail.com>
2022-10-27 13:56:21 +0800

    feat(main): expose `xframe_reactive::*` from the main crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod reactive {
+    pub use xframe_reactive::*;
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use reactive::create_root;
