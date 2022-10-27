fdc7d49db8c992f9ec2cee6e8341a16919c14a95
loichyan <loichyan@foxmail.com>
2022-10-27 20:48:00 +0800

    build: use `lld` linker

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A simple rust project.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+        llvmPkgs = pkgs.llvmPackages;
+        mkShell = pkgs.mkShell.override { stdenv = llvmPkgs.stdenv; };
+      in
+      {
+        devShells.default = mkShell (with pkgs; {
+          nativeBuildInputs = [
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+            llvmPkgs.bintools
+          ];
+          NIX_CFLAGS_LINK = "-fuse-ld=lld";
+          RUSTFLAGS = [ "-Clinker=clang" "-Clink-arg=-fuse-ld=lld" ];
+        });
+      }
+    )
+  ;
+}
